0bba52dede7d59134965526e9e36c566e5da4df6
newseru <santimir@protonmail.com>
2025-02-27 14:12:11 +0000

    chore: modify prettier styling.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,12 +4,12 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config([
-    { ignores: ['docs/**', 'benchmark/**'] },
-    {
-        files: ['**/*.ts'],
-        languageOptions: { globals: { ...globals.browser, ...globals.node } },
-        rules: eslint.configs.recommended.rules,
-    },
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
+  {
+    ignores: ['docs/**', 'benchmark/**'],
+    files: ['**/*.ts'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    rules: eslint.configs.recommended.rules,
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
 ]);
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,14 +2,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    test: {
-        testTimeout: 12000,
-        logHeapUsage: true,
-        coverage: {
-            extension: ['.ts'],
-            provider: 'v8',
-            include: ['src/**'],
-            skipFull: true,
-        },
+  test: {
+    testTimeout: 12000,
+    logHeapUsage: true,
+    coverage: {
+      extension: ['.ts'],
+      provider: 'v8',
+      include: ['src/**'],
+      skipFull: true,
     },
+  },
 });
