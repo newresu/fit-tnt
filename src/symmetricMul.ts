29edd8a3243836df6d48d999085dc4dc705266f6
newseru <santimir@protonmail.com>
2025-03-14 14:16:09 +0000

    refactor: move terms variable out, improve JSDoc.

--- src/symmetricMul.ts
+++ src/symmetricMul.ts
@@ -2,19 +2,20 @@ import { Matrix } from 'ml-matrix';
 
 /**
  * Multiply a matrix by its transpose.
- * The result is a square matrix.
  * To calculate `AtA` pass `At`, to calculate `AAt` pass `A`.
  *
- * We take advantage of the symmetry to reduce the number of operations.
+ * Uses symmetry and contiguity to increase speed.
+ *
  * @param {Matrix} B - The matrix to multiply by its transpose.
- * @returns {Matrix} The result of `B * B^t`.
+ * @returns {Matrix} Square matrix, result of `B * B^t`.
  */
 export function symmetricMul(B: Matrix) {
   const BBt = new Matrix(B.rows, B.rows);
   const diagonal: number[] = [];
 
-  let d;
+  let d, terms;
   for (let i = 0; i < B.rows; i++) {
+    //row i
     d = 0;
     // calculate diagonal value
     for (let t = 0; t < B.columns; t++) {
@@ -22,20 +23,19 @@ export function symmetricMul(B: Matrix) {
     }
     diagonal.push(d);
 
-    // go over cols of A, using rows of At
+    // row_i x all_prev_rows; filling up an L
+    // but we set both: L and L'
     for (let j = 0; j < i; j++) {
-      // go over other rows of A
-      let terms = 0;
+      terms = 0;
       for (let k = 0; k < B.columns; k++) {
+        // dot prod
         terms += B.get(i, k) * B.get(j, k);
       }
-      // another opts is to transpose add outside the loop
-      // but same time and less memory to just set both
       BBt.set(j, i, terms);
       BBt.set(i, j, terms);
     }
   }
-  // BBt.add(BBt.transpose());
+  // BBt.add(BBt.transpose()); // no speed up, more memory.
   for (let i = 0; i < B.rows; i++) {
     BBt.set(i, i, diagonal[i]);
   }
@@ -43,31 +43,27 @@ export function symmetricMul(B: Matrix) {
 }
 
 /**
- * Multiply a upper triangular by its transpose.
- * If you need `Lt=U` and `U * Lt`, then use this function, passing `U`
+ * Compute `U * L`.
+ *
  * Take advantage of symmetry.
- * @param {Matrix} L - The matrix to multiply by its transpose.
- * @returns {Matrix} The result of `B * B^t`.
-
- * If `B = At` then this calculates `AtA`.
+ * @param {Matrix} U - The upper triangular matrix to multiply by its transpose.
+ * @returns {Matrix} `U * L`
+ *
  */
 export function symmetricMulUpperLower(U: Matrix) {
   const R = new Matrix(U.rows, U.rows);
   const diagonal: number[] = [];
 
-  let d;
+  let d, terms;
   for (let i = 0; i < U.rows; i++) {
     d = 0;
-    // calculate diagonal value (by itself)
     for (let t = 0; t < U.columns; t++) {
       d += U.get(i, t) ** 2;
     }
     diagonal.push(d);
 
-    // go over cols of A, using rows of At
     for (let j = 0; j < i; j++) {
-      // go over other rows of A
-      let terms = 0;
+      terms = 0;
       for (let k = i; k < U.columns; k++) {
         terms += U.get(i, k) * U.get(j, k);
       }
